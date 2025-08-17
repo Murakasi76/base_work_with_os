d31ab9e93b6cc080ea5775527cb23c6b354cd800
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-08-17 07:54:24 +0200

    add some comments in file

--- ex_1.py
+++ ex_1.py
@@ -3,14 +3,15 @@ import os
 
 def work_with_dir(path_for_new_dir: str):
     try:
-        files = os.listdir(path_for_new_dir)
+        files: list[str] = os.listdir(path_for_new_dir)
     except FileNotFoundError:
         print("not found dir")
         return None
     with open(os.path.join(path_for_new_dir, "file_info.txt"), "w") as f:
         f.write(f"=== Files from {path_for_new_dir}===\n")
         if not files:
-            f.write("No files")
+            f.write("No files\n")
+            return None
         for file in files:
             if os.path.isfile(os.path.join(path_for_new_dir, file)):
                 f.write(file + "\n")
@@ -19,9 +20,11 @@ def work_with_dir(path_for_new_dir: str):
 
     
 if __name__ == "__main__":
+    # If OS Linux, MacOS
     if os.name == "posix":
         work_with_dir("/tmp")
         print("Done")
+    # if OS Windows
     else:
         work_with_dir("C:\\Download")
         print("Done")
